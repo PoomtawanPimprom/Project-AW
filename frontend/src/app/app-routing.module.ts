fac03eea9e67c84b3c8d30f820fdc82db9b5435b
NamChoco <numchoco2512@gmail.com>
2024-09-05 14:59:06 +0700

    edit login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { TestRoutingPageComponent } from './routing/test-routing-page/test-routing-page.component';
 import { Routing2Component } from './routing/routing2/routing2.component';
 import { DynamicRoutingComponent } from './routing/dynamic-routing/dynamic-routing.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
 
 const routes: Routes = [
  { path: 'routing1', component: TestRoutingPageComponent },
  { path: 'routing2', component: Routing2Component },
  { path: 'routing/:id', component: DynamicRoutingComponent },
+ { path: 'login', component: LoginRegisterComponent},
 ];
 
 @NgModule({
