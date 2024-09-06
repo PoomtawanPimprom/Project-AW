50ab3d4babcb13253d075f8c1504e8f64d446af2
Thitiporn2045 <142084816+Thitiporn2045@users.noreply.github.com>
2024-09-06 22:03:19 +0700

    menu + search friend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +37,8 @@ import { EventInfoComponent } from './pages/event-info/event-info.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     // provideClientHydration()
