e4ae4b52d3edb1a0f7120159b4bd6c08719926ad
siriphobmean <siriphobpps46@gmail.com>
2024-09-06 22:25:59 +0700

    Update Search for Event

--- frontend/src/app/pages/event-myevent/event-myevent.component.ts
+++ frontend/src/app/pages/event-myevent/event-myevent.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+interface Event {
+  id: number;
+  image: string;
+  name: string;
+  location: string;
+  date_time: string;
+}
+
 @Component({
   selector: 'app-event-myevent',
   templateUrl: './event-myevent.component.html',
@@ -8,15 +16,25 @@ import { Router } from '@angular/router';
 })
 export class EventMyeventComponent implements OnInit {
 
-  event: any = [
-    {"id":"1", "image":"https://www.hfocus.org/sites/default/files/styles/hfocus_super_cover/public/2023-03/xxkkalangkay.png?itok=Humh0up8", "name":"ออกกำลังกาย", "location":"สระสามแสน", "date_time":"05/09/2024 16:00 น."},
-    {"id":"4", "image":"https://static.thairath.co.th/media/dFQROr7oWzulq5Fa5BWCKVeaXmVds6InyaIHka3CpOoBGMJFPBuCg1R98iYZ0dy8ixu.jpg", "name":"ตกปลา", "location":"อ่างห้วยยาง", "date_time":"08/09/2024 20:00 น."},
+  event: Event[] = [
+    {"id": 1, "image":"https://www.hfocus.org/sites/default/files/styles/hfocus_super_cover/public/2023-03/xxkkalangkay.png?itok=Humh0up8", "name":"ออกกำลังกาย", "location":"สระสามแสน", "date_time":"05/09/2024 16:00 น."},
+    {"id": 4, "image":"https://static.thairath.co.th/media/dFQROr7oWzulq5Fa5BWCKVeaXmVds6InyaIHka3CpOoBGMJFPBuCg1R98iYZ0dy8ixu.jpg", "name":"ตกปลา", "location":"อ่างห้วยยาง", "date_time":"08/09/2024 20:00 น."},
   ]
+
+  filteredEvents: Event[] = [];
+  searchTerm: string = '';
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-      
+    this.filteredEvents = this.event;
+  }
+
+  onSearchEvent(): void {
+    this.filteredEvents = this.event.filter((f: Event) => {
+      const matchesSearch = f.name.includes(this.searchTerm);
+      return matchesSearch;
+    });
   }
 
   onAddEvent(): void {
