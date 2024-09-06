5075201682ad053ba0a2f5268df0762ee166035d
Thitiporn2045 <142084816+Thitiporn2045@users.noreply.github.com>
2024-09-06 21:15:04 +0700

    UI friend

--- frontend/src/app/pages/friend/friend.component.ts
+++ frontend/src/app/pages/friend/friend.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-friend',
   templateUrl: './friend.component.html',
   styleUrl: './friend.component.css'
 })
-export class FriendComponent {
+export class FriendComponent implements OnInit {
+
+
+  friend: any = [
+    {
+      "id": "1",
+      "image": "https://i.pinimg.com/736x/26/25/62/2625624d1249b9ca751e5ac2d9403d86.jpg",
+      "name": "สมพร กี่งใบทอง",
+      "faculty": "สำนักวิชาวิทยาศาสตร์",
+      "status": "accepted"
+    },
+    {
+      "id": "2",
+      "image": "https://i.pinimg.com/736x/26/25/62/2625624d1249b9ca751e5ac2d9403d86.jpg",
+      "name": "นวลน้อย ปรีชา",
+      "faculty": "สำนักวิชาวิศวกรรมศาสตร์",
+      "status": "accepted"
+    },
+    {
+      "id": "3",
+      "image": "https://i.pinimg.com/736x/26/25/62/2625624d1249b9ca751e5ac2d9403d86.jpg",
+      "name": "วีระ โชติพงศ์",
+      "faculty": "สำนักวิชาสังคมศาสตร์",
+      "status": "accepted"
+    },
+    {
+      "id": "4",
+      "image": "https://i.pinimg.com/736x/26/25/62/2625624d1249b9ca751e5ac2d9403d86.jpg",
+      "name": "มณีรัตน์ รัตนบรรเทิง",
+      "faculty": "สำนักวิชาการจัดการ",
+      "status": "pending"
+    },
+    {
+      "id": "5",
+      "image": "https://i.pinimg.com/736x/26/25/62/2625624d1249b9ca751e5ac2d9403d86.jpg",
+      "name": "ธนภัทร แสนเมือง",
+      "faculty": "สำนักวิชาศึกษาทั่วไป",
+      "status": "pending"
+    },
+    
+  ];
+  
+
+
+  institute!: string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  onFilterInstitute(receiver: string): void {
+    this.institute = receiver;
+    console.log("Filter: ", this.institute)
+  }
 
 }
