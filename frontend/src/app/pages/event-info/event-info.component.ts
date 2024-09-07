36f9a018b64a710b5c9d005b283424b809beb449
siriphobmean <siriphobpps46@gmail.com>
2024-09-07 15:14:01 +0700

    Update today and del m (mmm) poomta

--- frontend/src/app/pages/event-info/event-info.component.ts
+++ frontend/src/app/pages/event-info/event-info.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Event } from '../../interfaces/event.model';
 
 @Component({
   selector: 'app-event-info',
   templateUrl: './event-info.component.html',
   styleUrl: './event-info.component.css'
 })
-export class EventInfoComponent {
+export class EventInfoComponent implements OnInit {
 
+  // Get EventDataByID
+  event: Event[] = [
+    {"id": 1, "image":"https://www.hfocus.org/sites/default/files/styles/hfocus_super_cover/public/2023-03/xxkkalangkay.png?itok=Humh0up8", "name":"ออกกำลังกาย", "location":"สระสามแสน", "date_time":"05/09/2024 16:00 น."},
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+      
+  }
 }
--- frontend/src/app/pages/event/event.component.ts
+++ frontend/src/app/pages/event/event.component.ts
@@ -9,6 +9,7 @@ import { Event } from '../../interfaces/event.model';
 })
 export class EventComponent implements OnInit {
 
+  // GetListEvent
   event: Event[] = [
     {"id": 1, "image":"https://www.hfocus.org/sites/default/files/styles/hfocus_super_cover/public/2023-03/xxkkalangkay.png?itok=Humh0up8", "name":"ออกกำลังกาย", "location":"สระสามแสน", "date_time":"05/09/2024 16:00 น."},
     {"id": 2, "image":"https://www.amarinbabyandkids.com/app/uploads/2021/03/benefits-of-eating-together-with-family-feature-1024x538.jpg", "name":"กินข้าว", "location":"ร้าน ป.1/2", "date_time":"06/09/2024 17:00 น."},
