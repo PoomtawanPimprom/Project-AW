66c097d5c6c8fac090562e4e15c7eb4b9214e2b6
siriphobmean <siriphobpps46@gmail.com>
2024-09-07 21:50:02 +0700

    Add Pagination for MyEvent

--- frontend/src/app/pages/event-myevent/event-myevent.component.ts
+++ frontend/src/app/pages/event-myevent/event-myevent.component.ts
@@ -17,18 +17,62 @@ export class EventMyeventComponent implements OnInit {
 
   filteredEvents: Event[] = [];
   searchTerm: string = '';
+  currentPage: number = 1;
+  itemsPerPage: number = 6;
+  totalPages: number = 0;
+  maxVisiblePages: number = 4;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.filteredEvents = this.event;
+    this.totalPages = Math.ceil(this.filteredEvents.length / this.itemsPerPage);
+    this.updatePaginatedEvents();
   }
 
+  // onSearchEvent(): void {
+  //   this.filteredEvents = this.event.filter((f: Event) => {
+  //     const matchesSearch = f.name.includes(this.searchTerm);
+  //     return matchesSearch;
+  //   });
+  // }
   onSearchEvent(): void {
     this.filteredEvents = this.event.filter((f: Event) => {
-      const matchesSearch = f.name.includes(this.searchTerm);
-      return matchesSearch;
+      return f.name.includes(this.searchTerm);
     });
+    
+    if (this.filteredEvents.length >= this.itemsPerPage) {
+      this.totalPages = 1;
+    } else {
+      this.totalPages = Math.ceil(this.filteredEvents.length / this.itemsPerPage);
+    }
+    
+    this.currentPage = 1;
+  }
+
+  updatePaginatedEvents(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.filteredEvents = this.event.slice(startIndex, endIndex);
+  }
+
+  changePage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.updatePaginatedEvents();
+    }
+  }
+
+  getVisiblePages(): number[] {
+    const startPage = Math.max(1, this.currentPage - Math.floor(this.maxVisiblePages / 2));
+    const endPage = Math.min(this.totalPages, startPage + this.maxVisiblePages - 1);
+    const pages = [];
+
+    for (let i = startPage; i <= endPage; i++) {
+      pages.push(i);
+    }
+
+    return pages;
   }
 
   onEventInfo(eventId: string): void {
