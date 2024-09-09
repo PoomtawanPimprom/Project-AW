51cb00dccce09eec7ac7d1d17920db4d99fe6445
Thitiporn2045 <142084816+Thitiporn2045@users.noreply.github.com>
2024-09-09 21:04:48 +0700

    backend a little

---
+++ backend/router/friend.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Friend = require("../models/friend");
+
+// GET getFriend
+router.get('/', async (req, res) => {
+    try {
+        const data = await Friend.find(); //  Select * from friend
+        return res.json(data);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+});
+
+
