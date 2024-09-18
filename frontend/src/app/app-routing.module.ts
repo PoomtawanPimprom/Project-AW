1a62125f43810a29bb82fc988868ba1e22d8dce1
siriphobmean <siriphobpps46@gmail.com>
2024-09-19 00:01:08 +0700

    Delete folder routing for test

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TestRoutingPageComponent } from './routing/test-routing-page/test-routing-page.component';
-import { Routing2Component } from './routing/routing2/routing2.component';
-import { DynamicRoutingComponent } from './routing/dynamic-routing/dynamic-routing.component';
 import { MainComponent } from './pages/main/main.component';
 import { EventComponent } from './pages/event/event.component';
 import { EventInfoComponent } from './pages/event-info/event-info.component';
@@ -18,9 +15,6 @@ import { RegisterComponent } from './pages/register/register.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'routing1', component: TestRoutingPageComponent },
-  { path: 'routing2', component: Routing2Component },
-  { path: 'routing/:id', component: DynamicRoutingComponent },
   { path: 'main', component: MainComponent, canActivate: [AuthGuard] },
   { path: 'friend', component: FriendComponent, canActivate: [AuthGuard] },
   { path: 'event', component: EventComponent, canActivate: [AuthGuard] },
