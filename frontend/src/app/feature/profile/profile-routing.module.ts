ab11a9ffb383bc148d000380f7d4b52633d1f870
NamChoco <numchoco2512@gmail.com>
2024-09-22 08:52:02 +0700

    add AuthGuard function

--- frontend/src/app/feature/profile/profile-routing.module.ts
+++ frontend/src/app/feature/profile/profile-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile.component';
+import { AuthGuard } from '../../guards/auth.guard';
 
-const routes: Routes = [{ path: '', component: ProfileComponent }];
+const routes: Routes = [{ path: '', component: ProfileComponent, canActivate: [AuthGuard] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
